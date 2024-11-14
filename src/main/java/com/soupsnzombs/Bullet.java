803e896f931ba12b49ba92dd24b023541cd71c4d
Andre <339617284@gotvdsb.ca>
2024-11-14 12:42:33 -0500

    bullet class

---
+++ src/main/java/com/soupsnzombs/Bullet.java
@@ -0,0 +1,21 @@
+package com.soupsnzombs;
+
+import java.awt.*;
+
+public class Bullet extends Rectangle {
+    private int width, height, direction, speed = 10;
+
+    Bullet(int w, int h, int d) {
+        width = w;
+        height = h;
+        direction = d;
+    }
+
+    void update(int direction) {
+        
+    }
+
+    void draw() {
+
+    }
+}
