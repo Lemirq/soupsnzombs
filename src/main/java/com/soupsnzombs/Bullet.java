0809ade139847505ee648a7775e4d1253d9e0fca
Matthew <363301664@gotvdsb.ca>
2024-11-17 19:57:59 -0500

    finished importing pseudocode of bullet class

--- src/main/java/com/soupsnzombs/Bullet.java
+++ src/main/java/com/soupsnzombs/Bullet.java
@@ -2,20 +2,35 @@ package com.soupsnzombs;
 
 import java.awt.*;
 
-public class Bullet extends Rectangle {
-    private int width, height, direction, speed = 10;
 
-    Bullet(int w, int h, int d) {
+public class Bullet extends Rectangle {
+    private int width, height, speed = 10;
+
+    Bullet(int w, int h) {
         width = w;
         height = h;
-        direction = d;
     }
 
     void update(int direction) {
-        
+        switch (direction) {
+            //1: right, -1: left, 0: up, 2: down 
+            case 1:
+                x+=speed;
+                break;
+            case -1:
+                x-=speed;
+                break;
+            case 0:
+                y+=speed;
+                break;
+            case 2:
+                y-=speed;
+                break;
+        }
     }
 
-    void draw() {
-
+    void draw(Graphics2D g2) {
+        g2.setColor(Color.YELLOW);
+        g2.fillOval(x, y, width, height);
     }
 }
