742db068afc4bd238ae2a922948a904979b0d795
Matthew <363301664@gotvdsb.ca>
2024-12-11 13:06:43 -0500

    added 1 second "grace period" for gundrops fixed gundrop collisions with player

--- src/main/java/com/soupsnzombs/entities/GunDrop.java
+++ src/main/java/com/soupsnzombs/entities/GunDrop.java
@@ -1,29 +1,45 @@
 package com.soupsnzombs.entities;
 import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.Timer;
 
 import com.soupsnzombs.GamePanel;
 
 public class GunDrop extends Rectangle {
     Gun gunInfo;
     Color color;
+    Boolean interactable;
+    Timer gracePeriod; 
     public GunDrop(int x, int y, Gun gunToBeStored, Color c) {
         super(x, y, 30, 30);
         this.gunInfo = gunToBeStored;
         this.color = c;
+        interactable = false;
+        gracePeriod = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gracePeriod.stop();
+                interactable = true;
+            }
+        });
+        gracePeriod.start();
     }
 
     public void draw(Graphics2D g2d, Player player) {
         g2d.setColor(this.color);
         g2d.fillOval(this.x + GamePanel.offsetX, this.y + GamePanel.offsetY, this.width, this.height);
-
-        if (player.intersects(this)) {
-            GamePanel.gunDrops.remove(this);
-            player.dropGun();
-            player.setGun(gunInfo);
-            
-        }
-        
     }
 
-    
+    public Rectangle getBounds() {
+        return new Rectangle(super.x, super.y, super.width, super.height);
+    }
+
+    public Gun getGunInfo() {
+        return this.gunInfo;
+    }
+
+    public boolean isInteractable() {
+        return interactable;
+    }
 }
