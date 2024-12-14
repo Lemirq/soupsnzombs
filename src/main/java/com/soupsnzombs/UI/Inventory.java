31b807eedebf2dded646861ebc6d9b2739b89793
SlimyOintment <363301664@gotvdsb.ca>
2024-12-13 22:03:55 -0500

    added basic inventory system

--- src/main/java/com/soupsnzombs/UI/Inventory.java
+++ src/main/java/com/soupsnzombs/UI/Inventory.java
@@ -1,5 +1,16 @@
 package com.soupsnzombs.UI;
+import java.awt.*;
+
+import com.soupsnzombs.GamePanel;
+import com.soupsnzombs.entities.Gun;
 
 public class Inventory {
-
+    public void draw(Graphics2D g2d, GamePanel game, Gun gun) {
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(7));
+        g2d.setFont(new Font("DejaVuSans 12", Font.PLAIN, 20));
+        g2d.drawString("Current gun: ", game.getWidth()-150, game.getHeight()-200);
+        g2d.drawRect(game.getWidth()-133, game.getHeight()-185, 80, 80);
+        if (gun.getDamage() != 0) gun.getDrop().draw(g2d, game.getWidth()-108, game.getHeight()-160);
+    }
 }
