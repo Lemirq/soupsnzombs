9c00bc8e29e178aa9b444283e4a48faffc7ab0ad
Vihaan Sharma <86086039+Lemirq@users.noreply.github.com>
2025-01-16 12:59:03 -0500

    Refactor instruction text in NameSelect class

--- src/main/java/com/soupsnzombs/UI/MainMenu/NameSelect.java
+++ src/main/java/com/soupsnzombs/UI/MainMenu/NameSelect.java
@@ -134,7 +134,7 @@ public class NameSelect {
     public void drawInstructions(Graphics2D g2d) {
         g2d.setFont(FontLoader.font30);
         FontMetrics metrics = g2d.getFontMetrics(g2d.getFont());
-        String instructionText = "Enter a name - [B] to Enter/Skip, [C] to select";
+        String instructionText = "Enter a name - [B] to Enter/Skip, [C] to select word";
         int textWidth = metrics.stringWidth(instructionText);
         int xPosition = (GamePanel.screenWidth - textWidth) / 2;
         g2d.setColor(new Color(0, 102, 0));
